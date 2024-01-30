0267d30fa7a1583896acc7fcda5c95444f78cb9f
Paddy-McG <120601884+Paddy-McG@users.noreply.github.com>
2024-01-30 10:35:11 +0000

    Finishing up Day1 codeing

--- Day1.py
+++ Day1.py
@@ -89,3 +89,12 @@ reversed_name = name[::-1]
 print(first_name)
 print(last_name)
 print(funky_name)
+print(reversed_name)
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+
+slice = slice(7,-4)
+
+print(website1[slice])
+print(website2[slice])
