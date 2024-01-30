872578f087757522f6bfa5ce409a86c73dd38e67
Paddy-McG <120601884+Paddy-McG@users.noreply.github.com>
2024-01-30 11:53:06 +0000

    Creating sets

--- Day2.py
+++ Day2.py
@@ -107,4 +107,22 @@ for x in student:
 if "Lewis"in student:
     print("Lewis is here!")
     
+# ************************************** Sets ************************************** #
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowl", "plate", "cup", "knife"}
+
+utensils.add("napkin")
+utensils.remove("fork")
+utensils.clear()
+utensils.update(dishes)
+
+dinner_table = utensils.union(dishes)
+
+print(utensils.difference(utensils))
+print(utensils.intersection(dishes))
+
+for x in utensils:
+    print(x)
     
+for i in dinner_table:
+    print(i)
