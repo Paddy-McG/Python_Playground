3b7380969d75ca0e46d5d3ebfe68fb5aacba189c
Paddy-McG <120601884+Paddy-McG@users.noreply.github.com>
2024-01-31 10:22:32 +0000

    Index operating

---
+++ Day3.py
@@ -0,0 +1,18 @@
+
+# ************************************** Index Operator ************************************** #
+
+name = "lewis hamilton"
+
+if (name[0].islower()):
+    name = name.capitalize() # Capatalises the first letter
+    
+first_name = name[:5].upper()
+last_name = name[6:].lower()
+last_character = name[-1]
+
+print(last_character)
+print(last_name)
+print(first_name)
+print(name)
+
+# ************************************** Functions ************************************** #
