c406686f1093067ab130c5813d41c7a40f0cac9e
Paddy-McG <120601884+Paddy-McG@users.noreply.github.com>
2024-02-05 11:38:20 +0000

    Day4

--- Day4.py
+++ Day4.py
@@ -1,1 +1,29 @@
 
+# ************************************** String Formats ************************************** #
+
+animal = "cow"
+item = "moon"
+
+# print("The " + animal + " jumped over the " + item)
+print("The {} jumped over the {}".format(animal, item)) 
+print("The {1} jumped over the {0}".format(animal, item)) 
+print("The {star} jumped over the {an}".format(an = "Sheep", star = "Sun")) 
+
+text = "The {} jumped over the {}"
+
+print(text.format(animal, item))
+
+name = "Lewis"
+print("Hello my name is {}".format(name))
+print("Hello my name is {:10}.Nice To meet you.".format(name))
+print("Hello my name is {:>10}.Nice To meet you.".format(name))
+print("Hello my name is {:^10}.Nice To meet you.".format(name))
+
+number = 1000
+
+print("The number pie is {:.2f}".format(number)) # decimal points
+print("The number pie is {:,}".format(number)) 
+print("The number pie is {:b}".format(number)) #binary
+print("The number pie is {:o}".format(number)) # 
+print("The number pie is {:X}".format(number)) # Hexedecimal
+print("The number pie is {:E}".format(number)) # Scientific Notation
